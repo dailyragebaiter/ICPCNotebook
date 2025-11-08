fcb2cb06d91395409c35a10f4a1c1129454094d9
Brian the Crab <120788503+Crablegit@users.noreply.github.com>
2025-11-08 22:22:58 +0700

    Add files via upload

---
+++ content/Bay/TEMP.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define task   "htgdtctn"
+
+#define int    long long
+#define pii    pair <int, int>
+#define fi     first
+#define se     second
+#define szf    sizeof
+#define sz(s)  (int)((s).size())
+#define all(v) (v).begin(), (v).end()
+
+using namespace std;
+
+template <class T> void minimize (T &t, T f) {if (t > f) t = f;}
+template <class T> void maximize (T &t, T f) {if (t < f) t = f;}
+
+const int maxN = 2e5 + 5;
+const int inf = 1e18 + 7;
+const int mod = 1e9 + 7;
+
+// mt19937_64 rd (chrono :: steady_clock :: now ().time_since_epoch ().count ());
+// int Rand (int l, int r) {return uniform_int_distribution <int> (l, r) (rd);}
+
+inline void Solve () {
+    
+    return;
+}
+
+signed main () {
+    cin.tie (nullptr) -> sync_with_stdio (false);
+    if (fopen (task".inp", "r")) {
+        freopen (task".inp", "r", stdin);
+        freopen (task".out", "w", stdout);
+    }
+    int t = 1;
+    //cin >> t;
+    while (t --) {
+        Solve ();
+    } 
+    return 0;
+}
+// wake me up when September ends
