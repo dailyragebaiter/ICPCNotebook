acee71941ecab5f21c2b35d679c4bd10b55e5845
Brian the Crab <120788503+Crablegit@users.noreply.github.com>
2025-12-07 22:04:30 +0700

    Update DsuOnTree.cpp

--- content/Graph/DsuOnTree.cpp
+++ content/Graph/DsuOnTree.cpp
@@ -1,27 +1,3 @@
-#include <bits/stdc++.h>
-
-#define task   "htgdtctn"
-
-#define int    long long
-#define pii    pair <int, int>
-#define fi     first
-#define se     second
-#define szf    sizeof
-#define sz(s)  (int)((s).size())
-#define all(v) (v).begin(), (v).end()
-
-using namespace std;
-
-template <class T> void minimize (T &t, T f) {if (t > f) t = f;}
-template <class T> void maximize (T &t, T f) {if (t < f) t = f;}
-
-const int maxN = 2e5 + 5;
-const int inf = 1e18 + 7;
-const int mod = 1e9 + 7;
-
-// mt19937_64 rd (chrono :: steady_clock :: now ().time_since_epoch ().count ());
-// int Rand (int l, int r) {return uniform_int_distribution <int> (l, r) (rd);}
-
 int n;
 int c[maxN];
 int sz[maxN], big[maxN];
@@ -114,17 +90,4 @@ inline void Solve () {
     return;
 }
 
-signed main () {
-    cin.tie (nullptr) -> sync_with_stdio (false);
-    if (fopen (task".inp", "r")) {
-        freopen (task".inp", "r", stdin);
-        freopen (task".out", "w", stdout);
-    }
-    int t = 1;
-    //cin >> t;
-    while (t --) {
-        Solve ();
-    } 
-    return 0;
-}
-// wake me up when September ends
+
