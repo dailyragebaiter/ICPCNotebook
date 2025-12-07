1ff8de6f862bb4f67433dd6a3176dd46f5dc1066
Brian the Crab <120788503+Crablegit@users.noreply.github.com>
2025-12-07 22:05:03 +0700

    Refactor and clean up capGhepCucDai.cpp

--- content/Graph/capGhepCucDai.cpp
+++ content/Graph/capGhepCucDai.cpp
@@ -1,28 +1,3 @@
-#include <bits/stdc++.h>
-
-#define task     "BriantheCrab"
-
-#define int    long long
-#define pii    pair <int, int>
-#define fi     first
-#define se     second
-#define szf    sizeof
-#define sz(s)  (int)((s).size())
-#define all(v) (v).begin(), (v).end()
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-
-using namespace std;
-
-template <class T> void minimize (T &t, T f) {if (t > f) t = f;}
-template <class T> void maximize (T &t, T f) {if (t < f) t = f;}
-
-const int maxN = 2e5 + 5;
-const int inf = 1e18 + 7;
-const int mod = 1e9 + 7;
-
 int a[maxN];
 vector <int> adj[maxN];
 int V1, V2, E;
@@ -64,18 +39,3 @@ void solve () {
     
     return;
 }
-
-signed main () {
-    cin.tie (nullptr) -> sync_with_stdio (false);
-    if (fopen (task".inp", "r")) {
-        freopen (task".inp", "r", stdin);
-        freopen (task".out", "w", stdout);
-    }
-    int t = 1;
-    //cin >> t;
-    while (t --) {
-        solve ();
-    } 
-    return 0;
-}
-// thfv
